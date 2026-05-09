5cd3bc9ef2e15f139e224e2bf2c8205b9d09b026
myfabioxx-web <myfabioxx@gmail.com>
2026-05-09 16:26:27 +0700

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div style={{
+      background: "#0f0f0f",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1 style={{
+        color: "#facc15",
+        fontSize: "48px",
+        marginBottom: "20px"
+      }}>
+        สมองหลังเลิกงาน
+      </h1>
+
+      <p style={{
+        fontSize: "20px",
+        color: "#ccc"
+      }}>
+        เว็บไซต์กำลังออนไลน์ 🚀
+      </p>
+    </div>
+  )
+}
+
+export default App
