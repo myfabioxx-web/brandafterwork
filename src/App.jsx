1deb7d8ccc878cdfaa9caff633c0ad57f5d5be59
myfabioxx-web <myfabioxx@gmail.com>
2026-05-10 12:42:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
     {
       title: "เทคนิคโฟกัส ทำงานเยอะแต่เหนื่อยน้อยลง",
       image:
-        "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=1600&auto=format&fit=crop",
+        "/src/hero.png",
     },
     {
       title: "เริ่มต้นลงทุน สำหรับคนเงินเดือนประจำ",
